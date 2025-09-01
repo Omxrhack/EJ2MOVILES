1c07f0ba1f5ebc232f3c96057d0556300dfb8120
Omar Bermejo Osuna <l21170203@culiacan.tecnm.mx>
2025-08-31 20:01:16 -0700

    UI

--- EJ2MOVILES/ContentView.swift
+++ EJ2MOVILES/ContentView.swift
@@ -9,16 +9,70 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello,   PRUEBA!")
+        VStack{
+            HStack(){
+                Text("Comida")
+                     .font(.largeTitle)
+                     .foregroundColor(.blue)
+                     .fontWeight(.bold)
+                     .padding(.bottom, 10)
+            
+            }
+            
+            HStack{
+                ContenedorComida(T1Per: "Pizza" , ImgPer: "" , T2Per: "209")
+                ContenedorComida(T1Per: "Pizza" , ImgPer: "" , T2Per: "20")
+            }
+            Spacer()
+            HStack{
+                Text("Bebidas")
+                    .font(.largeTitle)
+                    .foregroundColor(.blue)
+                    .fontWeight(.bold)
+                    .padding()
+                
+       
+                
+
+            }
+          
         }
+    
         .padding()
     }
 }
 
+//A si se hace una estructura reutilizable
+struct ContenedorComida: View {
+    let T1Per: String
+    let ImgPer: String
+    let T2Per: String
+    var body: some View {
+        VStack{
+            Image(ImgPer)
+                .resizable()
+                .frame(maxWidth: .infinity, maxHeight: 300)
+                .cornerRadius(20)
+                .scaledToFit()
+                .padding()
+            Text(T1Per)
+                .fontWeight(.medium)
+                .font(.title)
+                .foregroundColor(.indigo)
+                .padding()
+            
+            HStack{
+                Spacer()
+            Text("$ \(T2Per)")
+                    .bold()
+            }
+        }
+        .padding()
+        .background(Color.gray.opacity(0.2))
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        
+    }
+}
 #Preview {
     ContentView()
 }
