4788d77c6347dbc0a79e2974bce3d33c8114775e
Omar Bermejo Osuna <l21170203@culiacan.tecnm.mx>
2025-08-31 19:12:59 -0700

    Initial Commit

---
+++ EJ2MOVILES/EJ2MOVILESApp.swift
@@ -0,0 +1,17 @@
+//
+//  EJ2MOVILESApp.swift
+//  EJ2MOVILES
+//
+//  Created by Omar Bermejo Osuna on 31/08/25.
+//
+
+import SwiftUI
+
+@main
+struct EJ2MOVILESApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
